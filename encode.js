04c48089e4561b2682f7b67e67e268d0fa54bfdc
Gordan Savicic <gordan.savicic@gmail.com>
2021-01-07 16:37:10 +0100

    new veresion

--- encode.js
+++ encode.js
@@ -5,18 +5,18 @@ const {
 
 
 
-var watcher = chokidar.watch('images/', {
-  ignored: /^\./,
+var watcher = chokidar.watch('./', {
+  ignored: ['**/node_modules/**/*', '**/.git/**/*','**/output/**/*'],
   persistent: true
 });
 
 watcher
   .on('add', function (path) {
     console.log('File', path, 'has been added');
-    if (path == "images/0000008.jpg") {
+    if (path == "./0000008.jpg") {
       console.log("create GIF now ... 3 .. 2 .. 1 ...")
       let newfilename = Date.now() + ".gif";
-      exec("ffmpeg -y -f image2 -i images/%07d.jpg output/" + newfilename, (error, stdout, stderr) => {
+      exec("ffmpeg -y -f image2 -i ./%07d.jpg output/" + newfilename, (error, stdout, stderr) => {
         if (error) {
           console.log(`error: ${error.message}`);
           return;
@@ -24,11 +24,10 @@ watcher
         if (stderr) {
           console.log(`stderr: ${stderr}`);
           console.log('---- remove old files....')
-          exec("rm images/*.jpg");
+          exec("rm ./*.jpg");
 
           return;
         }
-        console.log('---- done....')
         console.log(`stdout: ${stdout}`);
       });
 
@@ -36,14 +35,6 @@ watcher
   })
   .on('change', function (path) {
     console.log('File', path, 'has been changed');
-    if (path == "images/myanimated.gif") {
-
-
-      fs.readdir('.', (error, files) => {
-        if (error) throw error;
-        files.filter(name => /images\.\d+\.png$/.test(name)).forEach(fs.unlink);
-      });
-    }
   })
   .on('unlink', function (path) {
     console.log('File', path, 'has been removed');
